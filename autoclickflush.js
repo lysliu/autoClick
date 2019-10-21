772c6b991d81a4fac34165bed12985463d866b2a
Liu YanSong <lysliu@cn.ibm.com>
2019-10-21 15:49:47 +0800

    init create project

---
+++ autoclickflush.js
@@ -0,0 +1,19 @@
+﻿var timecount=0;
+var nums=[
+"18XXXXXXXXX",
+"18XXXXXXXXX"
+]
+function autoClick(){
+        var obj = new Date()
+        var curHour =  obj.getHours()
+        var curMinute = obj.getMinutes()
+        var curSecond = obj.getSeconds()
+        if( curHour == 9 || curHour ==14 && curMinute==53 && curSecond==59){
+                // if( curHour == 9 || curHour ==21 && curMinute==59 && curSecond==59){
+        
+        // $(".zhc-btn__content").click(); 
+        document.getElementById("lys").click()
+       } 
+}
+
+var interval=setInterval(autoClick,1000);
